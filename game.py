a6e6108054eaa590802d3bb1a163dff9546d9207
ZacharyRSmith <ZacharySmith4989@gmail.com>
2015-05-04 20:12:45 +0000

    Make method stubs for Game.

--- game.py
+++ game.py
@@ -1,2 +1,30 @@
+from line   import *
+from square import *
+from turn   import *
+
 class Game(object):
-    pass
+    def __init__(self):
+        print "Initializing Game object..."
+        self.crnt_plyr = "X"
+        # Make squares and lines and relationships
+        #
+        # Until turn_counter == end or victory, turn
+
+    def check_victory(self):
+        pass
+
+    def mark_square(self):
+        pass
+
+    def prompt_square(self):
+        pass
+
+    def switch_player(self):
+        pass
+
+    def turn(self):
+        pass
+        # Switch player
+        # prompt_sqr
+        # mark_sqr
+        # check_victory
