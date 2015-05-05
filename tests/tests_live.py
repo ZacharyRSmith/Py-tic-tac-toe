5e0e8228c93ec29b3eb03855287b3d088d114d47
ZacharyRSmith <ZacharySmith4989@gmail.com>
2015-05-05 19:29:38 +0000

    Get tests_live to pass.

--- tests/tests_live.py
+++ tests/tests_live.py
@@ -1,16 +1,18 @@
 from subprocess import Popen, PIPE, STDOUT
 
-f_in = open('in', 'r')        # If you have multiple test-cases, store each
+f_in = open('tests/in', 'r')        # If you have multiple test-cases, store each
                               # input/expected-output file in a list and iterate
                               # over this snippet.
-f_expected = open('out_expected', 'r')  # Correct outputs
+f_expected = open('tests/out_expected', 'r')  # Correct outputs
 expected   = f_expected.read().splitlines()
-p = Popen(["python", "../__init__.py"], stdin=PIPE, stdout=PIPE)
+p = Popen(["python", "__init__.py"], stdin=PIPE, stdout=PIPE)
 out = p.communicate(input=f_in.read())[0].splitlines()
 
 # Validate by comparing the two lists' elements.
+# print "out: ", out
+# print "expected: ", expected
 assert len(out) == len(expected)
 for i in range(len(out)):
-    print "out: ", out[i]
-    print "expected: ", expected[i]
+#     print "out: ", out[i]
+#     print "expected: ", expected[i]
     assert out[i] == expected[i]
